749cbfdaf17451fd19fe8b0a57c86295b7d295ff
David Liu <dliu@pinterest.com>
2017-01-13 01:16:00 -0800

    react router - for debug page

--- web/src/App.js
+++ web/src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import autobahn from 'autobahn';
 import './App.css';
 import { Line as LineChart } from 'react-chartjs-2';
+import { HashRouter, Match, Link } from 'react-router'
 
 class App extends Component {
   constructor() {
@@ -50,64 +51,77 @@ class App extends Component {
   }
 
   render() {
-    const vals = this.state.raw_log;
-    const data = {
-      labels: [...Array(vals.length).keys()],
-      datasets: [
-        {
-          label: 'raw sensor value',
-          fill: false,
-          lineTension: 0.1,
-          //backgroundColor: 'rgba(75,192,192,0.4)',
-          //borderColor: 'rgba(75,192,192,1)',
-          //borderCapStyle: 'butt',
-          //borderDash: [],
-          //borderDashOffset: 0.0,
-          //borderJoinStyle: 'miter',
-          //pointBorderColor: 'rgba(75,192,192,1)',
-          //pointBackgroundColor: '#fff',
-          //pointBorderWidth: 1,
-          //pointHoverRadius: 5,
-          //pointHoverBackgroundColor: 'rgba(75,192,192,1)',
-          //pointHoverBorderColor: 'rgba(220,220,220,1)',
-          //pointHoverBorderWidth: 2,
-          //pointRadius: 1,
-          //pointHitRadius: 10,
-          data: vals,
-        }
-      ]
-    };
-    const options = {
-        scales: {
-            xAxes: [{
-                display: false
-            }]
-        }
-    };
-    //<LineChart data={data} options={options} />
-    //<div className="CurrentPullups__pullups">
-    //  {this.props.data.raw_value}</div>
-
     return (
+      <HashRouter>
       <div className="App">
-        <header className="App__header">Pullup Standup</header>
+        <header className="App__header">
+          <Link to="/">Pullup Standup</Link>
+          <Link to="/debug" style={{float:'right'}}>[dbg]</Link>
+        </header>
 
-        {
-          this.state.session
-            ? [
-              <CurrentUser session={this.state.session}
-                           user={this.state.current_user}
-                           enroll_mode={this.state.enroll_mode} />,
-              this.state.current_user &&
-                <CurrentPullups session={this.state.session} data={this.state.pullup}
-                  raw_log={this.state.raw_log} />,
-            ]
-            : <h2>Disconnected.</h2>
-        }
+        <Match exactly pattern="/"
+               render={props => <KioskScreen {...props} state={this.state} />} />
+        <Match exactly pattern="/debug"
+               render={props => <DebugScreen {...props} state={this.state} />} />
       </div>
+      </HashRouter>
     );
   }
 }
+
+const KioskScreen = ({state}) => (
+  state.session
+    ? <div>
+    <CurrentUser session={state.session}
+                 user={state.current_user}
+                 enroll_mode={state.enroll_mode} />
+    {state.current_user &&
+    <CurrentPullups session={state.session} data={state.pullup} />}
+  </div>
+    : <h2>Disconnected.</h2>
+);
+
+const DebugScreen = ({state}) => {
+  const vals = state.raw_log;
+  const data = {
+    labels: [...Array(vals.length).keys()],
+    datasets: [
+      {
+        label: 'raw sensor value',
+        fill: false,
+        lineTension: 0.1,
+        //backgroundColor: 'rgba(75,192,192,0.4)',
+        //borderColor: 'rgba(75,192,192,1)',
+        //borderCapStyle: 'butt',
+        //borderDash: [],
+        //borderDashOffset: 0.0,
+        //borderJoinStyle: 'miter',
+        //pointBorderColor: 'rgba(75,192,192,1)',
+        //pointBackgroundColor: '#fff',
+        //pointBorderWidth: 1,
+        //pointHoverRadius: 5,
+        //pointHoverBackgroundColor: 'rgba(75,192,192,1)',
+        //pointHoverBorderColor: 'rgba(220,220,220,1)',
+        //pointHoverBorderWidth: 2,
+        //pointRadius: 1,
+        //pointHitRadius: 10,
+        data: vals,
+      }
+    ]
+  };
+  const options = {
+    scales: {
+      xAxes: [{
+        display: false
+      }]
+    }
+  };
+  return <div>
+    <LineChart data={data} options={options} />
+    <div className="CurrentPullups__pullups">
+      {state.pullup && state.pullup.raw_value}</div>
+  </div>;
+};
 
 export default App;
 
